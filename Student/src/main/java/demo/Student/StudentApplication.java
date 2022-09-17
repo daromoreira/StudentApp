03316872797c32e447a40edeed71917058761752
daromoreira <darylmq@gmail.com>
2022-09-17 19:48:55 -0300

    PR 01 test

--- Student/src/main/java/demo/Student/StudentApplication.java
+++ Student/src/main/java/demo/Student/StudentApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StudentApplication {
-
+//this is a test of the PR from visual studio code
 	public static void main(String[] args) {
 		SpringApplication.run(StudentApplication.class, args);
 	}
